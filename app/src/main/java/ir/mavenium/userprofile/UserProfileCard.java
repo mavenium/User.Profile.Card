096a3243a2e300dd5e10ef79f1eebc204ec58ba6
Mehdi Namaki <mavenium@gmail.com>
2019-09-19 23:40:21 +0430

    create custom view class

---
+++ app/src/main/java/ir/mavenium/userprofile/UserProfileCard.java
@@ -0,0 +1,15 @@
+package ir.mavenium.userprofile;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+public class UserProfileCard extends RelativeLayout {
+    public UserProfileCard(Context context) {
+        super(context);
+    }
+
+    public UserProfileCard(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+}
